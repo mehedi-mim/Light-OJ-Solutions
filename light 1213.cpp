d547cdf9de8a39924a4bb14a73fed10448590944
Mehedi Hasan Mim <MH-mim@users.noreply.github.com>
2018-08-15 22:33:22 +0600

    Add files via upload

---
+++ light 1213.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define pb            push_back
+#define mp            make_pair
+#define R             return 0
+#define ff            first
+#define ss            second
+#define sc1(a)        scanf("%d",&a)
+#define sc2(a,b)      scanf("%d%d",&a,&b)
+#define scl1(a)       scanf("%lld",&a)
+#define scl2(a,b)     scanf("%lld %lld",&a,&b)
+#define pnt(a)        printf("%d\n",a)
+#define pntl(a)       printf("%lld\n",a)
+#define all(a)        a.begin(),a.end()
+#define REP(it,a,b)   for(ll it = a;it < b;it++)
+#define pii           pair<int,int>
+#define pll           pair<long,long>
+#define hi            puts("Hi..!")
+#define MOD           1000000007LL
+#define MAX           1000009
+int fx[] = {1,-1,0,0};
+int fy[] = {0,0,1,-1};
+template <class T> inline T bigmod(T p,T e,T M){
+    ll ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return (T)ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+struct Sorter{int fs,sc;}; // Define array here before last ';'.
+bool cmp(Sorter A,Sorter B){if(A.fs==B.sc) return A.sc < B.sc;return A.fs < B.fs;}
+int arr[1000000];
+int main() {
+        ll T;
+        scl1(T);ll cs = 0;
+        while(T--){
+          ll ans = 0;
+          ll n,k,M;
+          scl2(n,k);scl1(M);
+          ll i = 1;
+          ll cum = 0;
+          while(i <= n){
+            ll x;scl1(x);
+            cum += x;
+            i++;
+          }
+          ll keep = ((k%M)*(cum%M)%M);
+          ans = bigmod(n,k-1,M);
+          ans = (ans * keep) % M;
+          printf("Case %lld: %lld\n",++cs,ans);
+        }
+}
+/*
+*/
+
