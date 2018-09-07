0622835f6865a90830b741153636dbd541458f8a
Mehedi Hasan Mim <MH-mim@users.noreply.github.com>
2018-09-07 18:54:18 +0600

    Add files via upload

---
+++ light 1005.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define ld            long double
+#define pb            push_back
+#define m_p            make_pair
+#define ff            first
+#define ss            second
+#define sc1(a)        scanf("%d",&a)
+#define sc2(a,b)      scanf("%d%d",&a,&b)
+#define scl1(a)       scanf("%lld",&a)
+#define scl2(a,b)     scanf("%lld %lld",&a,&b)
+#define pnt(a)        printf("%d\n",a)
+#define pntl(a)       printf("%lld\n",a)
+#define all(a)        a.begin(),a.end()
+#define rep(it,a,b)   for(ll it = a;it < b;it++)
+#define pii           pair<int,int>
+#define pll           pair<long,long>
+#define hi            puts("Hi..!")
+#define MOD           1000000007LL
+#define MAX           1000009
+int fx[] = {1,-1,0,0};
+int fy[] = {0,0,1,-1};
+template <class T> inline T bigmod(T p,T e,T M){
+    ll ret = 1;
+    for(; e > 0; e >>= 1){
+        if(e & 1) ret = (ret * p) % M;
+        p = (p * p) % M;
+    } return (T)ret;
+}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+template <class T> inline T power(T p,T e){ll ret = 1;for(; e > 0; e >>= 1){if(e & 1) ret = (ret * p);p = (p * p);}return (T)ret;}
+struct Sorter{int fs,sc;}; // Define array here before last ';'.
+bool cmp(Sorter A,Sorter B){if(A.fs==B.sc) return A.sc < B.sc;return A.fs < B.fs;}
+int toInt(string s){int sm;stringstream ss(s);ss>>sm;return sm;}
+int toLlint(string s){long long int sm;stringstream ss(s);ss>>sm;return sm;}
+
+//solution...............
+int main() {
+   ll T;scl1(T);ll cs = 0;
+   while(T--){
+    ll ans = 0;
+    ll nPr = 1,nCr = 1;
+    ll n,k;
+    scl2(n,k);
+    if(k <= n){
+        ll mn = min(k,n-k);
+        for(ll i = n;i > max(k,n-k);i--) nCr *= i;
+        ll div = 1;
+        rep(i,1,min(k,n-k)+1) div *= i;
+        nCr = nCr/div;
+        for(ll i = n;i > (n-k);i--) nPr *= i;
+        //cout << nCr <<" " << nPr << endl;
+        ans = nPr*nCr;
+    }
+    printf("Case %lld: %lld\n",++cs,ans);
+   }
+   return 0;
+}
+/*
+
+*/
+
