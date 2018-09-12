e13a24d51fe69beb7517a04aa74d82ef7e0da8ee
Mehedi Hasan Mim <MH-mim@users.noreply.github.com>
2018-09-12 23:02:08 +0600

    Add files via upload

---
+++ light 1102.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll            long long
+#define ld            long double
+#define pb            push_back
+#define m_p           make_pair
+#define ff            first
+#define ss            second
+#define sc1(a)        scanf("%d",&a)
+#define sc2(a,b)      scanf("%d%d",&a,&b)
+#define scl1(a)       scanf("%lld",&a)
+#define scl2(a,b)     scanf("%lld %lld",&a,&b)
+#define pnt(a)        printf("%d\n",a)
+#define pntl(a)       printf("%lld\n",a)
+#define all(a)        a.begin(),a.end()
+#define rep(it,a,b)   for(ll it = a;it < b;it++)
+#define pii           pair<int,int>
+#define pll           pair<long,long>
+#define hi            puts("Hi..!")
+#define MOD           1000000007LL
+#define MAX           1000009
+int fx[] = {1,-1,0,0};
+int fy[] = {0,0,1,-1};
+template <class T> inline T power(T p,T e){ll ret=1;for(;e>0;e>>=1){if(e&1)ret=(ret*p);p=(p*p);}return(T)ret;}
+template <class T> inline T bigmod(T p,T e,T M){ll ret=1;for(;e>0;e>>=1){if(e&1)ret=(ret*p)%M;p=(p*p)%M;}return(T)ret;}
+template <class T> inline T modinverse(T a,T M){return bigmod(a,M-2,M);}
+template <class T> inline T gcd(T a,T b){if(b==0)return a;return gcd(b,a%b);}
+struct Sorter{int fs,sc;}; // Define array here before last ';'.
+bool cmp(Sorter A,Sorter B){if(A.fs==B.sc) return A.sc < B.sc;return A.fs < B.fs;}
+int toInt(string s){int sm;stringstream ss(s);ss>>sm;return sm;}
+int toLlint(string s){long long int sm;stringstream ss(s);ss>>sm;return sm;}
+
+//solution...............
+ll fact[3000009];
+
+void ImMim()
+{
+    fact[0] = 1;
+    for(ll i = 1;i <= 3000000;i++){
+        fact[i] = (fact[i-1]*i)%MOD;
+    }
+}
+int main() {
+   ImMim();
+   int T;sc1(T);int cs = 0;
+   while(T--){
+    ll n,k,ans = 0;
+    scl2(n,k);
+    ll Nu,deno;
+    Nu = fact[n+k-1];
+    deno = (fact[n]*fact[k-1])%MOD;
+    deno = modinverse(deno,MOD);
+    ans = (Nu*(deno)%MOD)%MOD;
+    printf("Case %d: %lld\n",++cs,ans);
+   }
+   return 0;
+}
+/*
+4
+4 3
+3 5
+1000 3
+1000 5
+*/
+
+
